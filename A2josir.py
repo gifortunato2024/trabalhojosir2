3653db418d7e7ea73146174a96c690058d12f944
gifortunato2024 <168582782+gifortunato2024@users.noreply.github.com>
2024-06-15 22:29:00 -0300

    Update A2josir.py

--- A2josir.py
+++ A2josir.py
@@ -12,7 +12,7 @@ st.sidebar.info("Nosso site é para os amantes de podcast que estão em busca de
 page = st.session_state.get("page", 1)
 
 if page == 1:
-    st.title("Amantes de Podcast")
+    st.title("Amantes de Podcast ❤️")
     foto = Image.open('Foto site .JPG')
     st.image(foto, width=500)
     st.header("Vamos conhecer seus hábitos de assistir podcasts!")
@@ -21,14 +21,14 @@ if page == 1:
     canais_diferentes = st.radio("Você costuma assistir canais diferentes?", ("Sim", "Não, gosto de assistir o mesmo sempre "))
 
     if assiste_podcast == "Sim, amo!":
-        st.header("Se sim, que bom! Vou te mostrar outros para você experimentar. Se você só assiste os mesmos, essa é uma ótima oportunidade para conhecer novos canais.")
+        st.header("Se sim, que bom! Vou te mostrar outros para você experimentar. Se você só assiste os mesmos, essa é uma ótima oportunidade para conhecer novos canais.🤩")
         if st.button("Next"):  # Verifica se o botão "Next" foi pressionado
             st.session_state["page"] = 2
 
 # Página 2: Perguntar sobre o nicho de interesse
 if page == 2:
     st.title("Nicho de Podcast")
-    st.header("Qual nicho de podcast você gosta de assistir?")
+    st.header("Qual nicho de podcast você gosta de assistir? 🎤")
     nicho = st.selectbox("Escolha um nicho", ["Conversas", "Empreendedorismo", "Educacao", "Esporte", "Jogos", "Tecnologia", "Noticias"])
     nome_arquivo = "podcast_" + nicho.lower() + ".csv"
     # Mostrar  canais de podcast relacionados ao nicho escolhido
